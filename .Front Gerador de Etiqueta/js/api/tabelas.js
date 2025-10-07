6cd7a30bd819f5f697862454ff3fd58a530445c0
Massaki19989 <massakidragon@gmai.com>
2025-10-07 11:54:38 -0300

    Atualizações incompletas

--- .Front Gerador de Etiqueta/js/api/tabelas.js
+++ .Front Gerador de Etiqueta/js/api/tabelas.js
@@ -15,6 +15,30 @@ async function comparar(url, formData) {
     }
 }
 
+async function juntar(url, formData) {
+    try{
+        const response = await fetch(url, {
+            method: 'POST',
+            body: formData
+        });
+        if(!response.ok) throw new Error(`Erro: ${response.status} - ${response.statusText}`);
+
+        const result = await response.json();
+        return result;
+    }catch(e){
+        console.error(e);
+    }
+}
+
+document.querySelector('#together').addEventListener('submit', function(e){
+    e.preventDefault();
+    const formData = new FormData(this);
+    console.log(formData);
+    juntar('http://localhost:5000/excel/together', formData).then(data => {
+        console.log(data);
+    });
+});
+
 document.querySelector('#compare').addEventListener('submit', function(e){
     e.preventDefault();
 
